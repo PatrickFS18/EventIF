5bf5487c55afe3df98308c22d2b94a7a05d1f701
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2023-07-07 14:21:24 +0000

    ok

--- core/tests.py
+++ core/tests.py
@@ -2,4 +2,11 @@ from django.test import TestCase
 
 
 class TestHome(TestCase):
-    def test_
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_home(self):
+        self.assertEqual(self.response.status_code, 200)
+    def test_template_used(self):
+        
+        self.assertTemplateUsed(self.response, 'index.html')
