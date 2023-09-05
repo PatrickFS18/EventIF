5eb331915125336ca336f96cb65b6892d18d2f09
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2023-09-05 22:28:32 +0000

    mudei para createdat

--- contact/models.py
+++ contact/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
     email = models.EmailField()
     mensagem = models.TextField()
     telefone = models.CharField(max_length=15, blank=True, null=True)
-    data_envio = models.DateTimeField(auto_now_add=True)
+    CreatedAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nome
