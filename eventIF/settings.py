814726a2f120731146b7eda10ba846901a4c5d25
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2023-09-06 23:33:09 +0000

    configurando mail

--- eventIF/settings.py
+++ eventIF/settings.py
@@ -37,6 +37,15 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = ['127.0.0.1','.localhost']... urls do sistema
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST  = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 
 
 # Application definition
--- subscriptions/tests/test_mail_subscribe.py
+++ subscriptions/tests/test_mail_subscribe.py
@@ -4,7 +4,7 @@ from django.core import mail
 
 class MailTest(TestCase):
     def setUp(self):
-        data = dict(name='Patrick Souza', cpf='054001000230', email='whesleysouza21@gmail.com', phone='53999999999')
+        data = dict(name='Patrick Souza', cpf='054001000230', email='patrick.souza@aluno.riogrande.ifrs.edu.br', phone='53999999999')
         self.response = self.client.post('/inscricao/', data)
         self.email = mail.outbox[0]
         
@@ -13,15 +13,15 @@ class MailTest(TestCase):
         self.assertEqual(expect, self.email.subject)
 
     def test_subscription_email_sender(self):
-        expect = 'whesleysouza21@gmail.com'
+        expect = 'contato@eventif.com.br'
         self.assertEqual(expect, self.email.from_email)
     
     def test_subscription_email_to(self):
-        expect = ['whesleysouza21@gmail.com', 'whesleysouza21@gmail.com']  # Atualize os endereços de e-mail aqui
+        expect = ['contato@eventif.com.br', 'patrick.souza@aluno.riogrande.ifrs.edu.br']  # Atualize os endereços de e-mail aqui
         self.assertEqual(expect, self.email.to)
     
     def test_subscription_email_body(self):
-        contents = ['Patrick Souza','054001000230', 'whesleysouza21@gmail.com', '53999999999']
+        contents = ['Patrick Souza','054001000230', 'patrick.souza@aluno.riogrande.ifrs.edu.br', '53999999999']
         for content in contents:
             with self.subTest():
                 self.assertIn(content, self.email.body)
