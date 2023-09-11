5ba51bc1306fb71c861d700b58cb28f89ad6f65f
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2023-09-11 21:46:58 +0000

    url adicionada contact

--- eventif/urls.py
+++ eventif/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from core.views import home
 from subscriptions.views import subscribe
-
+from contact.views import contato_view
 urlpatterns = [
     path('', home),
     path('inscricao/', subscribe),
     path("admin/", admin.site.urls),
+    path("contact/",contato_view),
+
 ]
